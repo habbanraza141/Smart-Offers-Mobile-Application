46ab17ef6cea17f850dabf2c3ed1c7ff8c4a52df
muhammad_habban_raza <habbanraza@gmail.com>
2023-11-15 08:30:51 +0500

    work done

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import {  SafeAreaView, Text,  View,} from 'react-native';
+
+
+function App() {
+return (
+    <SafeAreaView >
+    <Text>Habban Raza</Text>
+    </SafeAreaView>
+  );
+}
+
+
+
+export default App;
